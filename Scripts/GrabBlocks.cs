15720d495014b257763e10c41b88bf384f6b9bfe
Asante Dawkins <Amdawk48@users.noreply.github.com>
2015-10-16 15:21:24 -0400

    The script i got. The grab isn't working quite yet.

---
+++ Scripts/GrabBlocks.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrabBlocks : MonoBehaviour {
+
+    public bool holding = false;
+    public GameObject grabbedBlock;
+    public GameObject p;
+    Ray myRay;
+    private Vector3 rayOffset;
+    private Vector3 blockOffset;
+    private float distance = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+        myRay = new Ray(p.transform.position, p.transform.forward);
+        rayOffset = new Vector3(0,1,0);
+        blockOffset = new Vector3(0, 2, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.DrawRay(p.transform.position + rayOffset, p.transform.forward * distance);
+
+	   // if(Input.GetKey(KeyCode.Space)){
+            grab();
+       // }
+	}
+
+    void grab()
+    {
+        if (holding)
+        {
+            grabbedBlock.transform.position = p.transform.position + blockOffset;
+        }
+    }
+
+}
---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float movementSpeed = 5.0f;
+    public float tSpeed = 50.0f;
+    public bool rotating = false;
+    public bool holding = false;
+    public GameObject c;
+    public Rigidbody rb; 
+    public Animator child;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        child = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.position += rb.transform.forward * Time.deltaTime * movementSpeed;
+            child.Play("Walking");
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.position -= rb.transform.forward * Time.deltaTime * movementSpeed;
+            child.Play("Walking");
+        }
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
+        {
+            //child.Play("Walking");
+        }else{
+            //child.Play("Idle");
+        }
+   
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.transform.Rotate(Vector3.down);
+           rb.transform.Rotate(0,rb.GetComponent<Transform>().rotation.y - 1.0f,0);
+       
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.transform.Rotate(Vector3.up);
+            rb.transform.Rotate(0, rb.GetComponent<Transform>().rotation.y + 1.0f, 0);
+
+        }
+
+        if (Input.GetKey(KeyCode.Space)){
+            child.Play("Jump");
+            rb.AddForce(Vector3.up);
+        }
+
+
+    }
+}
